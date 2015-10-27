fe24608b2787f8fdf43fb6d7bb9fd9c3024a1558
Dirk Peeters <ponystal-peeters@telenet.be>
2015-10-27 23:42:58 +0100

    Update UnderWaterFog.cs

--- UnderWaterFog.cs
+++ UnderWaterFog.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 //This script enables underwater fog effect. Attach the script to the main camera and set the fog color.
 
-public class UnderWaterFog : MonoBehavior
+public class UnderWaterFog : MonoBehaviour
 {
     // Underwater fog color
     public Color fogColor;
