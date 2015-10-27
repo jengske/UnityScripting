8d9be730b8df4312cb17663b8c33371866e9672a
Dirk Peeters <ponystal-peeters@telenet.be>
2015-10-27 23:42:14 +0100

    Create Lit.cs

---
+++ Lit.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class Lit : MonoBehaviour
+{
+  public static string MainCharacter = "MC"; // modify this to your player tag
+  public static string Respawn = "Respawn"; // respawn point
+}
