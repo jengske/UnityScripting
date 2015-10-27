155461f864dbd2896ed9e91952c27f83d63c60a4
Dirk Peeters <ponystal-peeters@telenet.be>
2015-10-28 00:05:31 +0100

    version 1

--- CatchAll.cs
+++ CatchAll.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 // This script destroys all objects in range, used for scenes were objects could fall true the ground
 // Create a emptyGameObject, add a boxCollider, set it to trigger, attach the script.
-
+// you could expand it with a Respawn so the objects respawn back insted of being destroyed
 public class CatchAll : MonoBehaviour
 {
 private int totalDestroyed;
+private bool isDebug = false;
 
   void Start()
   {
     totalDestroyed = 0;
   }
+  
   void OnTriggerEnter(Collider otherId)
   {
     // On trigger enter, destroy otherId
@@ -19,6 +21,9 @@ private int totalDestroyed;
     // counter to register the destroyed objects
     totalDestroyed = totalDestroyed + 1;
     // print a Debug.log to the console
-    Debug.Log("TotalDestroyed", totalDestroyed);
+      if(true == isDebug)
+      {
+        Debug.Log("TotalDestroyed", totalDestroyed);
+      }
   }
 }
