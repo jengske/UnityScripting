fa8f1768b63cd6d55c5d5c76d1916f0cdc81ece7
Dirk Peeters <ponystal-peeters@telenet.be>
2015-10-27 22:54:18 +0100

    Update UnderWaterFog.cs

--- UnderWaterFog.cs
+++ UnderWaterFog.cs
@@ -1,1 +1,66 @@
+using UnityEngine;
+using System.Collections;
 
+//This script enables the underwater fog effect. Attach the script to the main camera.
+
+public class UnderWaterFog : MonoBehavior
+{
+    // Underwater fog color
+    public Color fogColor;
+    // The waterplane transform
+    public Transform waterplane;
+    // The terrain transform
+    public Transform terrain;
+    // The max density of the underwater fog
+    public float maxDensityFog = 0.1;
+    // Save the scene fog settings
+    private bool saveFogEnabledFlag;
+    private Color saveFogColor;
+    private float saveFogDensity;
+    private Material saveSkyboxMaterial;
+    // Check if we are under water
+    private bool isUnderWater = false;
+    // the level of the water surface
+    private float underwaterLevel;
+    // Use this for initilizations
+    void Start() 
+    {
+      // Set camera background color
+      camera.backgroundColor = fogColor;
+      // Save the scene default fog settings
+      saveFogEnabledFlag = RenderSettings.fog;
+      saveFogColor = RenderSettings.fogColor;
+      saveFogDensity = RenderSettings.fogDensity;
+      saveSkyboxMaterial = RenderSettings.skybox;
+      // Cache the water surface level
+      underwaterLevel = water.position.y;
+      if (terrain == null) 
+      {
+        terrain = null;
+      }
+      
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // check if the camera is under water
+        if (transform.position.y < underwaterLevel)
+        {
+            // enable the fog
+            RenderSettings.fog = true;
+            // set fog color
+            RenderSettings.fogColor = fogColor;
+            // set fog density
+            RenderSettings.maxFogDensity = maxFogDensity;
+            // turn the skybox off
+            RenderSettings.skybox = null;
+            // We are under water
+            isUnderWater = true;
+        }
+        // the main camera is not underwater
+        else
+        {
+          
+        }
+}
