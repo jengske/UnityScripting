64a050e78af38a4f98323cf9c39aded2f84e4539
Dirk Peeters <ponystal-peeters@telenet.be>
2015-10-27 23:15:30 +0100

    Empty unity script

---
+++ Empty.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+//This script enables underwater fog effect. Attach the script to the main camera and set the fog color.
+
+public class Empty : MonoBehavior
+{
+  void Start () 
+  {
+  
+  }
+  
+  void Update()
+  {
+  
+  }
+}
